8afe09a18327bf3edd5c6716423d3142a6d372fc
rileydevine <824055@mystma.org>
2021-09-28 09:03:24 -0500

    Submitted

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #WRITE YOUR CODE HERE
+
+#define addition checker
+
+def addCheck(x,y,z):
+    
+    #define equals
+    if x+y == z:
+        return True
+
+    #define elif
+    else:
+        return False
+
+#print function
+print(addCheck(5, 6, 10))
